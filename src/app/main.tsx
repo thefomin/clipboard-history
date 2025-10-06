91e82c691e6ab661505307e036d4d3ea43d8174c
thefomin <raveyt@yandex.ru>
2025-10-07 00:18:37 +0700

    fix: added router provider

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 
-import App from './app.tsx'
-import './index.css'
+import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>
 )
