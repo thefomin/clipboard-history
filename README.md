84d909fbbf6f161f963bfc2843b8a49fb706b91f
thefomin <raveyt@yandex.ru>
2025-10-06 23:23:37 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clipboard History
+
+A Chrome extension that keeps track of everything you copy — safely, locally, and instantly accessible.
---
+++ eslint.boundaries.js
@@ -0,0 +1,72 @@
+import boundaries from 'eslint-plugin-boundaries'
+
+export const eslintBoundariesConfig = {
+	plugins: {
+		boundaries
+	},
+	settings: {
+		//работают абсолютные импорты
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true
+			}
+		},
+		//размещаем архитектурное решение
+		'boundaries/elements': [
+			{
+				type: 'app',
+				pattern: './src/app'
+			},
+			{
+				type: 'features',
+				pattern: './src/features/*'
+			},
+			{
+				type: 'shared',
+				pattern: './src/shared'
+			}
+		]
+	},
+	//запрет на размещение модулей друг с другом
+	rules: {
+		'boundaries/element-types': [
+			2,
+			{
+				default: 'allow',
+				rules: [
+					{
+						from: 'shared',
+						disallow: ['app', 'features'],
+						message:
+							'Модуль нижележащего слоя (${file.type}) не может импортировать модуль вышележащего слоя (${dependency.type})'
+					},
+					{
+						from: 'features',
+						disallow: ['app'],
+						message:
+							'Модуль нижележащего слоя (${file.type}) не может импортировать модуль вышележащего слоя (${dependency.type})'
+					}
+				]
+			}
+		],
+		'boundaries/entry-point': [
+			2,
+			{
+				default: 'disallow',
+				message:
+					'Модуль (${file.type}) должен импортироваться через public API. Прямой импорт из ${dependency.source} запрещен',
+
+				rules: [
+					{
+						target: ['shared', 'app'],
+						allow: '**'
+					},
+					{
+						target: ['features'],
+						allow: ['index.(ts|tsx)', '*.page.tsx']
+					}
+				]
+			}
+		]
+	}
+}
